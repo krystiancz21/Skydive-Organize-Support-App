8a7ec8c6a18890b88df6c8977b0c6d9db700f680
czajkakacper <125756834+czajkakacper@users.noreply.github.com>
2023-10-31 12:58:24 +0100

    Poprawione tworzenie konta

--- frontend/src/components/EmployeeCreateAccount/index.jsx
+++ frontend/src/components/EmployeeCreateAccount/index.jsx
@@ -175,20 +175,110 @@ const EmployeeCreateAccount = () => {
                                                 />
                                             </Col>
                                         </Form.Group>
-                                        {/* <Form.Group as={Row} controlId="formOwnerCreateAccountRole" className="mb-3">
+                                    </div>
+
+                                    {createUserSuccess && <div className="alert alert-success">Pomyślnie udało się utworzyć konto nowego użytkownika!</div>}
+                                    {error && <div className="alert alert-danger">{error}</div>}
+                                    <Button variant="success" type="submit">UTWÓRZ KONTO</Button>
+                                </Form>
+                            </Container></>
+                    )}
+                    {userRole === 'admin' && (
+                        <><Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                            <Container>
+                                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                                <Navbar.Collapse id="responsive-navbar-nav">
+                                    <Nav className="me-auto">
+                                        <Nav.Link href="/main"><BiHomeAlt /></Nav.Link>
+                                        <Nav.Link href="/offer">OFERTA</Nav.Link>
+                                        <Nav.Link href="/messages">WIADOMOŚCI</Nav.Link>
+                                        <Nav.Link href="/employeemanagejumps">ZARZĄDZANIE SKOKAMI</Nav.Link>
+                                        <Nav.Link href="/owner-financial-overview">PODSUMOWANIE FINANSOWE</Nav.Link>
+                                    </Nav>
+                                    <Nav.Link href="/userprofile"><Navbar.Brand><AiOutlineUser /> {mail}</Navbar.Brand></Nav.Link>
+                                    <Button variant="danger" onClick={handleLogout}>WYLOGUJ</Button>
+                                </Navbar.Collapse>
+                            </Container>
+                        </Navbar>
+                            <Container className={styles.content}>
+                                <h1 className="text-center">TWORZENIE KONTA UŻYTKOWNIKA</h1>
+                                <Form onSubmit={handleSubmit} className="text-center">
+                                    <div className='max-width-form'>
+                                        <Form.Group as={Row} controlId="formOwnerCreateAccountName" className="mb-3">
                                             <Form.Label column sm={2}>
-                                                Rola
+                                                Imię
                                             </Form.Label>
-                                            <Col md={10} className="form-col">
-                                                <Form>
-                                                    <div className='przyciskiradio'>
-                                                        <label class="radio-inline">
-                                                            <input type="radio" name="myRadio" value="userAccount" checked disabled />  Użytkownik
-                                                        </label>
-                                                    </div>
-                                                </Form>
+                                            <Col sm={10}>
+                                                <FormControl
+                                                    type="name"
+                                                    placeholder="Jan"
+                                                    name="firstName"
+                                                    onChange={handleChange}
+                                                    value={data.firstName}
+                                                    required
+                                                />
                                             </Col>
-                                        </Form.Group> */}
+                                        </Form.Group>
+                                        <Form.Group as={Row} controlId="formOwnerCreateAccountLastName" className="mb-3">
+                                            <Form.Label column sm={2}>
+                                                Nazwisko
+                                            </Form.Label>
+                                            <Col sm={10}>
+                                                <FormControl
+                                                    type="surname"
+                                                    placeholder="Kowalski"
+                                                    name="lastName"
+                                                    onChange={handleChange}
+                                                    value={data.lastName}
+                                                    required
+                                                />
+                                            </Col>
+                                        </Form.Group>
+                                        <Form.Group as={Row} controlId="formOwnerCreateAccountEmail" className="mb-3">
+                                            <Form.Label column sm={2}>
+                                                E-mail
+                                            </Form.Label>
+                                            <Col sm={10}>
+                                                <FormControl
+                                                    type="email"
+                                                    placeholder="jankowalski@email.com"
+                                                    name="email"
+                                                    onChange={handleChange}
+                                                    value={data.email}
+                                                    required
+                                                />
+                                            </Col>
+                                        </Form.Group>
+                                        <Form.Group as={Row} controlId="formOwnerCreateAccountPassword" className="mb-3">
+                                            <Form.Label column sm={2}>
+                                                Hasło
+                                            </Form.Label>
+                                            <Col sm={10}>
+                                                <FormControl
+                                                    type="password"
+                                                    placeholder="********"
+                                                    name="password"
+                                                    onChange={handleChange}
+                                                    value={data.password}
+                                                    required
+                                                />
+                                            </Col>
+                                        </Form.Group>
+                                        <Form.Group as={Row} controlId="formOwnerCreateAccountPhone" className="mb-3">
+                                            <Form.Label column sm={2}>
+                                                Telefon
+                                            </Form.Label>
+                                            <Col sm={10}>
+                                                <FormControl
+                                                    type="phone"
+                                                    placeholder="666777888"
+                                                    name="phoneNumber"
+                                                    onChange={handleChange}
+                                                    value={data.phoneNumber}
+                                                    required
+                                                />
+                                            </Col>
+                                        </Form.Group>
                                     </div>
 
                                     {createUserSuccess && <div className="alert alert-success">Pomyślnie udało się utworzyć konto nowego użytkownika!</div>}
