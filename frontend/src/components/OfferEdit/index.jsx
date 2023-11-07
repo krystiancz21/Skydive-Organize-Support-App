8fa43464d9877d6fa03297c264e520cc828a5bde
czajkakacper <125756834+czajkakacper@users.noreply.github.com>
2023-11-07 15:18:39 +0100

    Update index.jsx

--- frontend/src/components/OfferEdit/index.jsx
+++ frontend/src/components/OfferEdit/index.jsx
@@ -123,8 +123,8 @@ const OfferEdit = () => {
                                 <Nav className="me-auto">
                                     <Nav.Link href="/main"><BiHomeAlt /></Nav.Link>
                                     <Nav.Link href="/messages">WIADOMOŚCI</Nav.Link>
-                                    <Nav.Link href="/employeeusersaccounts">KONTA UŻYTKOWNIKÓW</Nav.Link>
-                                    <Nav.Link href="/employeemanagejumps">ZARZĄDZANIE SKOKAMI</Nav.Link>
+                                    <Nav.Link href="/employee-users-accounts">KONTA UŻYTKOWNIKÓW</Nav.Link>
+                                    <Nav.Link href="/employee-manage-jumps">ZARZĄDZANIE SKOKAMI</Nav.Link>
                                     <Nav.Link href="/owner-financial-overview">PODSUMOWANIE FINANSOWE</Nav.Link>
                                 </Nav>
                                 <Nav.Link href="/userprofile"><Navbar.Brand><AiOutlineUser /> {mail}</Navbar.Brand></Nav.Link>
